212a54474b466d3abe091069d8eab97269e13e51
Rahmi <magicra84@gmail.com>
2020-12-05 00:34:39 -0300

    gmi project

---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,27 @@
+package stepdefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        //System.out.println("setUp Çalıştı.");
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if(scenario.isFailed()){
+            scenario.embed(screenshot , "image/png");
+        }
+        Driver.closeDriver();
+    }
+
+
+
+}
