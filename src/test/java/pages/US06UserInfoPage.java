3c1a16649116c27d71b5f3e9c626b0c3ea1b7561
fcakgul <69127151+fcakgul@users.noreply.github.com>
2020-12-08 00:19:30 -0500

    en son

--- src/test/java/pages/US06UserInfoPage.java
+++ src/test/java/pages/US06UserInfoPage.java
@@ -1,4 +1,35 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US06UserInfoPage {
+
+    public US06UserInfoPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(id="firstName")
+    public WebElement firstNameTxtBox;
+
+    @FindBy(id="lastName")
+    public WebElement lastNameTxtBox;
+
+    @FindBy(id="email")
+    public WebElement emailTxtBox;
+
+    @FindBy(id="langKey")
+    public WebElement langKeyTxtBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement saveButton;
+
+
+
+
+
+
 }
