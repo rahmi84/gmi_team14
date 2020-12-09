aa4e683f6a9c3c211fee79f71902a99ecbca9806
Rahmi <magicra84@gmail.com>
2020-12-08 22:41:47 -0300

    update

--- src/test/java/pages/US14CreateANewAccountPage.java
+++ src/test/java/pages/US14CreateANewAccountPage.java
@@ -1,4 +1,13 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US14CreateANewAccountPage {
+    public US14CreateANewAccountPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
 }
