4e1cd683fc8a3a805bf3749cbd466395ca8a5b2b
Said <said.fserik@gmail.com>
2020-12-13 14:54:00 -0500

    feature update

--- src/test/java/runners/RunnerGMI.java
+++ src/test/java/runners/RunnerGMI.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "stepdefinitions",
 
-     //  tags = "@db",
+      tags = "@US16",
         dryRun = false
 
         //  tags = "@db",
