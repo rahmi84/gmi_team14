4c86a0cee21dca2e99a1e9f9a793cf72a277b6ce
Rahmi <magicra84@gmail.com>
2020-12-14 13:45:26 -0300

    .'

--- src/test/java/runners/RunnerGMI.java
+++ src/test/java/runners/RunnerGMI.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "stepdefinitions",
 
-     //  tags = "@db",
+        tags="@US15_SmokeTest",
         dryRun = false
 
         //  tags = "@db",
