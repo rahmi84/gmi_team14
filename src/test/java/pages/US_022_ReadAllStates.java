d88795543e8e7cad95c678651eda6fa8519bfd75
Emine <btrusa45@gmail.com>
2020-12-16 20:24:06 -0600

    .

---
+++ src/test/java/pages/US_022_ReadAllStates.java
@@ -0,0 +1,5 @@
+package pages;
+
+public class US_022_ReadAllStates {
+    
+}
---
+++ src/test/java/runners/US_022_Runner.java
@@ -0,0 +1,25 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+
+        //  tags = "@db",
+        dryRun = false
+
+        //  tags = "@db",
+        // dryRun = true
+
+)
+public class US_022_Runner {
+
+
+}
