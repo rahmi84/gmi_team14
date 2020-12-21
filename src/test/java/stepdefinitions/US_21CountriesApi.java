8584e17dc71f6c424a8dffcc5d124490c7df9d4d
Said <said.fserik@gmail.com>
2020-12-20 20:23:29 -0500

    US_21 step definition update

--- src/test/java/stepdefinitions/US_21CountriesApi.java
+++ src/test/java/stepdefinitions/US_21CountriesApi.java
@@ -9,9 +9,16 @@ import io.restassured.response.Response;
 import pojo.Country;
 import utilities.ConfigReader;
 
+import java.io.IOException;
+
 import static io.restassured.RestAssured.*;
 
 public class US_21CountriesApi {
+
+    Response response;
+    String filePath=ConfigReader.getProperty("api_countries");
+    String bearerToken=ConfigReader.getProperty("token");
+    Country[] country;
 
     @Given("user read all countries and sets response using api end point {string}")
     public void user_read_all_countries_and_sets_response_using_api_end_point(String api_endpoint) {
@@ -26,14 +33,20 @@ public class US_21CountriesApi {
                 .statusCode(200)
                 .extract()
                 .response();
-        response.prettyPrint();
+     //   response.prettyPrint();
 
     }
 
     @Given("user deserialization countries data json to pojo")
-    public void user_deserialization_countries_data_json_to_pojo() {
+    public void user_deserialization_countries_data_json_to_pojo() throws IOException {
 
         ObjectMapper objectMapper=new ObjectMapper();
+        country=objectMapper.readValue(response.asString(),Country[].class);
+        for (int i=0; i<country.length;i++){
+            System.out.println("name"+" "+country[i].getName());
+
+        }
+
 
     }
 
