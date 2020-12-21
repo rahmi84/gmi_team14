461f9b7dbf3c6f50bca3ab9df98e1114346979ea
Rahmi <magicra84@gmail.com>
2020-12-21 18:24:09 -0300

    updates

--- src/test/java/stepdefinitions/US_26ApiStepDefinitions.java
+++ src/test/java/stepdefinitions/US_26ApiStepDefinitions.java
@@ -25,25 +25,24 @@ public class US_26ApiStepDefinitions extends ApiUtil {
 
     @Given("user goes to end point and change any user's country")
     public void user_goes_to_end_point_and_change_any_user_s_country() {
-
-        ApiUtil.actualCountry(18946);
+        ApiUtil.actualCountry(18945);
         System.out.println(".............");
 
-        ApiUtil.changeCountry(18946,"kenya",null);
+        ApiUtil.changeCountry(18945,"argentina",null);
     }
 
     @Then("verifies if actual one is updated")
     public void verifies_if_actual_one_is_updated() {
         Map<String,Object> req2= new HashMap<>();
-        req2.put("id",18946);
-        req2.put("name","kenya");
+        req2.put("id",18945);
+        req2.put("name","argentina");
         req2.put("states",null);
         Response response=given().
                 spec(spec01).
                 auth().oauth2(ConfigReader.
                 getProperty("token")).
                 when().
-                get("/18946");
+                get("/18945");
         Map<String ,Object>actual=response.as(HashMap.class);
         Assert.assertEquals(req2.get("name"),actual.get("name").toString());
 
