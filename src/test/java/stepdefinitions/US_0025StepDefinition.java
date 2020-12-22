3ffb2e34e1089763324ecade8b92fa9bc508c9dc
Dilara Bilir <dilarabilir70@gmail.com>
2020-12-22 13:25:18 -0500

    Us_3

--- src/test/java/stepdefinitions/US_0025StepDefinition.java
+++ src/test/java/stepdefinitions/US_0025StepDefinition.java
@@ -1,5 +1,4 @@
 package stepdefinitions;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,64 +7,59 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import utilities.ConfigReader;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 import static io.restassured.RestAssured.*;
-
-
 public class US_0025StepDefinition {
     Response response;
     JsonPath json;
-    Map<String, Object> expectedcountryMap = new HashMap<String,Object>();
-
+    Map<String, Object> expectedcountry = new HashMap<>();
     @Given("create a country with map and send to end point{string}")
     public void createACountryWithMapAndSendToEndPoint(String endpoint) {
-        expectedcountryMap.put("id", " ");
-        expectedcountryMap.put("name", " Ulsan1");
-        expectedcountryMap.put("states", null);
-        System.out.println(expectedcountryMap);
+        expectedcountry.put("id", " ");
+        expectedcountry.put("name", "Mongolia3");
+        expectedcountry.put("states", null);
+        System.out.println(expectedcountry);
         Response expectedCountry =  response = given()
                 .auth()
                 .oauth2(ConfigReader.getProperty("token"))
                 .contentType(ContentType.JSON)
                 .when()
-                .body(expectedcountryMap)
+                .body(expectedcountry)
                 .post(endpoint);
         response.prettyPrint();
-
         expectedCountry    .then()
-                           .contentType(ContentType.JSON)
-                           .statusCode(201);
+                .contentType(ContentType.JSON)
+                .statusCode(201);
         expectedCountry.prettyPrint();
     }
-
-
     @And("get response and store response with JsonPath using endpoint{string}")
     public void getResponseAndStoreResponseWithJsonPathUsingEndpoint(String url) {
-
-
-         response = given()
-                              .auth()
-                              .oauth2(ConfigReader.getProperty("token"))
-                              .contentType(ContentType.JSON)
-                              .get(url);
+        response = given()
+                .auth()
+                .oauth2(ConfigReader.getProperty("token"))
+                .contentType(ContentType.JSON)
+                .get(url);
         response.prettyPrint();
         json = response.jsonPath();
-
     }
-
-    @Then("validate the country that created before")
+    @And("validate the country that created before")
     public void validate_the_country_that_created_before() {
-        List<Object> id = json.getList("id");
-       if (json.getString("id") != null) {
-            Assert.assertEquals(id.contains(expectedcountryMap.get("60130")));
-       }
+        String id = json.getString("id");
+        System.out.println(id);
+        if (id != null) {
+            Assert.assertTrue("country is exist",id.contains("60767"));
+        }
     }
-//    String id = json.getString("id");
-//        if (id != null) {
-//        Assert.assertTrue("country is exist",id.contains((CharSequence)expectedcountry.get("id")));
-//    }
+
+
+    @Then("delete the country that created before using end point{string} with extension{string}")
+    public void deleteTheCountryThatCreatedBeforeUsingEndPointWithExtension(String endpoint, String id) {
+        Response expectedCountry =  response = given()
+                .auth()
+                .oauth2(ConfigReader.getProperty("token"))
+                .contentType(ContentType.JSON)
+                .when()
+                .delete(endpoint+id);
     }
+}
