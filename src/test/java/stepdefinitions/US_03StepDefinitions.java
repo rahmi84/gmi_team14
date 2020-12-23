001baa9c211d826fc9093b985acabe3ab34ea1b5
Dilara Bilir <dilarabilir70@gmail.com>
2020-12-22 20:57:13 -0500

    user stories

--- src/test/java/stepdefinitions/US_03StepDefinitions.java
+++ src/test/java/stepdefinitions/US_03StepDefinitions.java
@@ -1,115 +1,79 @@
-//package stepdefinitions;
-//
-//import io.cucumber.java.en.And;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import jdk.nashorn.internal.runtime.regexp.joni.Config;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.ui.Select;
-//import pages.HomePage;
-//import pages.US01$2RegisterPage;
-//import utilities.ConfigReader;
-//import utilities.Driver;
-//
-//public class US_03StepDefinitions {
-//    HomePage homePage = new HomePage();
-//    US01$2RegisterPage registerPage = new US01$2RegisterPage();
-//
-//    @FindBy(xpath = "//input[@name='ssn']")
-//    private WebElement ssnButton;
-//
-//    @Given("user at the home page")
-//    public void user_at_the_home_page() {
-//        Driver.getDriver().get(ConfigReader.getProperty("url"));
-//    }
-//
-//    @And("user clicks menu")
-//    public void userClicksMenu() throws InterruptedException {
-//        homePage.userMenu.click();
-//        homePage.userRegistrationButton.click();
-//    }
-//
-//    @And("user at the registration page and clicks ssn box and sends the ssnNumber")
-//    public void userAtTheRegistrationPageAndClicksSsnBoxAndSendsTheSsnNumber() {
-//
-//        registerPage.setSsn("123-58-4587");
-//    }
-//
-//    @Given("user clicks firstName box and sends the firstName")
-//    public void user_clicks_firstName_box_and_sends_the_firstName() {
-//        registerPage.setFirstName("Deniz");
-//
-//    }
-//
-//    @Given("user clicks lastName box and sends the lastName")
-//    public void user_clicks_lastName_box_and_sends_the_lastName() {
-//
-//        registerPage.setLastName("Ocean");
-//    }
-//
-//    @Given("user clicks address box and sends the address")
-//    public void user_clicks_address_box_and_sends_the_address() {
-//        registerPage.setAddress("350 Edgewood Dr.Oxford, MS 38655");
-//    }
-//
-//    @Given("user clicks phoneNumber box and sends the phoneNumber")
-//    public void user_clicks_phoneNumber_box_and_sends_the_phoneNumber() {
-//        registerPage.setMobilePhoneNumber("365-485-2578");
-//    }
-//
-//
-//    @Given("user clicks userName box and sends the userName")
-//    public void user_clicks_userName_box_and_sends_the_userName() {
-//        registerPage.setUsername("DenizOcean");
-//    }
-//
-//    @Given("user clicks email box and sends the email")
-//    public void user_clicks_email_box_and_sends_the_email() {
-//
-//        registerPage.setEmail("DenizOcean@gmail.com");
-//    }
-//
-//    @Given("user clicks password box and sends the password")
-//    public void user_clicks_password_box_and_sends_the_password() {
-//        String password = "Kk1@ljr";
-//        for (int i = 0; i < password.length(); i++) {
-//            if (password.charAt(i) >= 'a' && password.charAt(i) <= 'z') {
-//                System.out.println("There is one lowercase");
-//                continue;
-//                if (password.charAt(i) >= 'A' && password.charAt(i) <= 'Z') {
-//                    System.out.println("There is one uppercase");
-//                    continue;
-//                    if (password.charAt(i) >= '!' && password.charAt(i) <= '/') {
-//                        (password.charAt(i) >= ':' && password.charAt(i) <= '@')
-//
-//                    }
-//                    System.out.println("There is one digit");
-//                    continue;
-//                    if (password.charAt(i) >= 'A' && password.charAt(i) <= 'Z') {
-//                        System.out.println("There is one Uppercase");
-//                        continue;
-//
-//
-//                    }
-//
-//                    registerPage.setNewPassword(password);
-//
-////        There should be at least 1 lowercase char for stronger password and
-////        see the level chart change accordingly
-////        There should be at least 1 lowercase char and see the level  chart change accordingly
-////        There should be at least 1 digit  and see the level  chart change accordingly
-////        There should be at least 1 special char and see the level bar change accordingly
-////    There should be at least 7 chars for a stronger password
-//
-//                }
-//            }
-//        }
-//    }
-//    @Then("check the password if it meets with the acceptance criterias")
-//    public void checkThePasswordIfItMeetsWithTheAcceptanceCriterias() {
-//    }
-//
-//
-//}
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.HomePage;
+import pages.US01$2RegisterPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US_03StepDefinitions {
+    HomePage homePage = new HomePage();
+    US01$2RegisterPage registerPage = new US01$2RegisterPage();
+
+
+    @Given("user at the home page")
+    public void user_at_the_home_page() {
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+    }
+
+    @And("user clicks menu")
+    public void userClicksMenu() throws InterruptedException {
+        homePage.userMenu.click();
+        homePage.userRegistrationButton.click();
+    }
+
+    @And("user at the registration page and clicks ssn box and sends the ssnnumber {string}")
+    public void userAtTheRegistrationPageAndClicksSsnBoxAndSendsTheSsnnumber(String ssn) {
+        registerPage.ssn.sendKeys(ssn);
+    }
+
+    @And("user clicks firstName box and sends the firstname {string}")
+    public void userClicksFirstNameBoxAndSendsTheFirstname(String firstname) {
+        registerPage.firstName.sendKeys(firstname);
+    }
+
+    @And("user clicks lastName box and sends the  lastName {string}")
+    public void userClicksLastNameBoxAndSendsTheLastName(String lastname) {
+        registerPage.lastname.sendKeys(lastname);
+    }
+
+    @And("user clicks address box and sends the  address {string}")
+    public void userClicksAddressBoxAndSendsTheAddress(String address) {
+        registerPage.Address.sendKeys(address);
+    }
+
+    @And("user clicks phoneNumber box and sends the phoneNumber {string}")
+    public void userClicksPhoneNumberBoxAndSendsThePhoneNumber(String phoneNumber) {
+        registerPage.MobilePhoneNumber.sendKeys(phoneNumber);
+
+    }
+
+    @And("user clicks userName box and sends the  userName {string}")
+    public void userClicksUserNameBoxAndSendsTheUserName(String userName) {
+        registerPage.username.sendKeys(userName);
+    }
+
+    @And("user clicks email box and sends the  email {string}")
+    public void userClicksEmailBoxAndSendsTheEmail(String email) {
+        registerPage.Email.sendKeys(email);
+    }
+
+    @And("user clicks the password box and sends the  password {string}")
+    public void userClicksThePasswordBoxAndSendsThePassword(String password) {
+        registerPage.NewPassword.sendKeys(password);
+    }
+
+    @And("user clicks the password box and sends the secondPassword {string}")
+    public void userClicksThePasswordBoxAndSendsTheSecondPassword(String secondPassword) {
+        registerPage.NewPasswordConfirmation.sendKeys(secondPassword);
+    }
+
+    @Then("user clicks the RegisterButton")
+    public void userClicksTheRegisterButton() {
+        registerPage.register.click();
+    }
+}
+
+
--- src/test/resources/features/US_0025.feature
+++ src/test/resources/features/US_0025.feature
@@ -4,6 +4,6 @@ Feature: US_25
   Given create a country with map and send to end point"https://www.gmibank.com/api/tp-countries"
     And get response and store response with JsonPath using endpoint"https://www.gmibank.com/api/tp-countries"
   And validate the country that created before
-    Then delete the country that created before using end point"https://www.gmibank.com/api/tp-countries" with extension"/60767"
+#    Then delete the country that created before using end point"https://www.gmibank.com/api/tp-countries" with extension"/60767"
 
 
