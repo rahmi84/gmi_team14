364763eeaa3c5af2423ecf46377b03ae16b7a6f1
Rahmi <magicra84@gmail.com>
2020-12-26 01:35:14 -0300

    updated

--- src/test/java/pages/US06UserInfoPage.java
+++ src/test/java/pages/US06UserInfoPage.java
@@ -11,21 +11,48 @@ public class US06UserInfoPage {
     public US06UserInfoPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-
     @FindBy(id = "firstName")
     public WebElement firstNameTxtBox;
-
     @FindBy(id = "lastName")
     public WebElement lastNameTxtBox;
-
     @FindBy(id = "email")
     public WebElement emailTxtBox;
-
     @FindBy(id = "langKey")
     public WebElement langKeyTxtBox;
-
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement saveButton;
-
-
+    @FindBy(xpath = "//*[@id='settings-form']/div[1]/div")
+    public WebElement firstNameErrorMessage;
+    @FindBy(xpath = "//*[@id='settings-form']/div[2]/div")
+    public WebElement lastNameErrorMessage;
+    @FindBy(xpath = "//*[@id='settings-form']/div[3]/div")
+    public WebElement emailErrorMessage;
+    MainEmployeePage mainEmployeePage= new MainEmployeePage();
+    MainUserPage mainUserPage=new MainUserPage();
+    MainAdminPage mainAdminPage = new MainAdminPage();
+    MainCustomerPage mainCustomerPage= new MainCustomerPage();
+    public void IamAtUserInfoPage(String user06) {
+        switch (user06) {
+            case "employee":
+                Driver.loginAll("employee");
+                Driver.clickWithTimeOut(mainEmployeePage.accountMenu,2);
+                Driver.clickWithTimeOut(mainEmployeePage.drpUserInfo,2);
+                break;
+            case "customer":
+                Driver.loginAll("customer");
+                Driver.clickWithTimeOut(mainCustomerPage.accountMenu,2);
+                Driver.clickWithTimeOut(mainEmployeePage.drpUserInfo,2);
+                break;
+            case "admin":
+                Driver.loginAll("admin");
+                Driver.clickWithTimeOut(mainAdminPage.accountMenu,2);
+                Driver.clickWithTimeOut(mainAdminPage.drpUserInfo,2);
+                break;
+            case "user":
+                Driver.loginAll("user");
+                Driver.clickWithTimeOut(mainUserPage.team14_new_userButton,2);
+                Driver.clickWithTimeOut(mainUserPage.UserInfoButton,2);
+                break;
+        }
+    }
 }
