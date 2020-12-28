69e5428ad101607dc9ef3315fbdb1495af321bd7
bayramguney <bayramguney@yahoo.com>
2020-12-27 20:35:23 -0500

    son hali pdfUtil eklenip icine method konuldu

--- src/test/java/stepdefinitions/US_20StepDefinitions.java
+++ src/test/java/stepdefinitions/US_20StepDefinitions.java
@@ -18,14 +18,13 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
-import utilities.ApiUtil;
-import utilities.ConfigReader;
-import utilities.DatabaseUtility;
-import utilities.Driver;
+import pages.Deneme;
+import utilities.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -199,64 +198,9 @@ public class US_20StepDefinitions {
 
         String query = "Select first_name,last_name,middle_initial,phone_number,ssn from tp_customer order by id desc ";
         List<List<Object>> list = DatabaseUtility.getQueryResultList(query);
+        List<String>header= Arrays.asList("Firstname", "Lastname", "Middlename", "phone number", "ssn");
 
-        String[] headers = new String[]{"Firstname", "Lastname", "Middlename", "phone number", "ssn"};
-
-        String[][] rows = new String[10][5];
-        for (int i = 0; i < 10; i++) {
-            rows[i][0] = list.get(i).get(0).toString();
-            rows[i][1] = list.get(i).get(1).toString();
-            rows[i][2] = list.get(i).get(2).toString();
-            rows[i][3] = list.get(i).get(3).toString();
-            rows[i][4] = list.get(i).get(4).toString();
-
-
-        }
-        Document document = new Document(PageSize.LETTER.rotate());
-
-        try {
-
-            PdfWriter.getInstance(document,
-                    new FileOutputStream(new File("GmiBank.pdf")));
-            document.open();
-            Font fontTitle = new Font(Font.FontFamily.TIMES_ROMAN, 20);
-
-            document.add(new Paragraph("                                               " + "TEAM 14 USER INFO", fontTitle));
-            document.add(new Paragraph("          "));
-            document.add(new LineSeparator());
-            document.add(new Paragraph("          "));
-
-            Font fontHeader = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
-            Font fontRow = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.BOLD);
-
-            PdfPTable table = new PdfPTable(headers.length);
-            for (String header : headers) {
-                PdfPCell cell = new PdfPCell();
-                cell.setGrayFill(0.9f);
-                cell.setPhrase(new Phrase(header.toUpperCase(), fontHeader));
-                table.addCell(cell);
-            }
-            table.completeRow();
-
-            for (String[] row : rows) {
-                for (String data : row) {
-                    Phrase phrase = new Phrase(data, fontRow);
-                    table.addCell(new PdfPCell(phrase));
-                }
-                table.completeRow();
-            }
-
-            document.addTitle("PDF Table Demo");
-            document.add(table);
-            document.add(new Paragraph("          "));
-            document.add(new LineSeparator());
-            document.add(Image.getInstance("ny.jpg"));
-
-        } catch (DocumentException | IOException e) {
-            e.printStackTrace();
-        } finally {
-            document.close();
-        }
+        pdfUtil.createPdf("GmiBank.pdf","Team 14 Customer Info",20,"ny.jpg",list,header);
 
         DatabaseUtility.closeConnection();
     }
